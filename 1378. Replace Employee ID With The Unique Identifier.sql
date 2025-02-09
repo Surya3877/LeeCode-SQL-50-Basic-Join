feb717d0cc6ae35015fc6152fcda211059a3279f
Surya <valmikisuriya@gmail.com>
2025-02-09 20:11:35 +0530

    Update 1378. Replace Employee ID With The Unique Identifier.sql

--- 1378. Replace Employee ID With The Unique Identifier.sql
+++ 1378. Replace Employee ID With The Unique Identifier.sql
@@ -98,7 +98,14 @@ insert into EmployeeUNI (id, unique_id) values ('90', '3');
 
 SELECT * FROM employeeUNI;
 
+/*
+Write a solution to show the unique ID of each user, If a user does not have a unique ID replace just show null.
+
+Return the result table in any order.
+*/
+
 # SQL Query
+ 
 SELECT 
     EmployeeUNI.unique_id, 
     Employees.name
