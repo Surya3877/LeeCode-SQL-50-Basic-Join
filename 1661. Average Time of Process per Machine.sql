f050a51f5cbf4693059a44ed5b970d6c4f2e4a71
Surya <valmikisuriya@gmail.com>
2025-02-09 18:31:42 +0530

    Update 1661. Average Time of Process per Machine.sql

--- 1661. Average Time of Process per Machine.sql
+++ 1661. Average Time of Process per Machine.sql
@@ -96,10 +96,10 @@ INSERT INTO Activity (machine_id, process_id, activity_type, timestamp) VALUES (
 
 SELECT 
     a.machine_id, 
-	ROUND(AVG(b.timestamp - a.timestamp), 3) AS processing_time
+    ROUND(AVG(b.timestamp - a.timestamp), 3) AS processing_time
 FROM 
     Activity a, 
-	Activity b
+    Activity b
 WHERE 
     a.machine_id = b.machine_id
 AND 
