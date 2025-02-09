1ff5a16278c3a24fe4f63219c1188722e996918e
Surya <valmikisuriya@gmail.com>
2025-02-09 17:36:01 +0530

    Update 1581. Customer Who Visited but Did Not Make Any Transactions.sql

--- 1581. Customer Who Visited but Did Not Make Any Transactions.sql
+++ 1581. Customer Who Visited but Did Not Make Any Transactions.sql
@@ -75,6 +75,9 @@ Customer with id = 54 visited the mall three times. During 2 visits they did not
 Customer with id = 96 visited the mall once and did not make any transactions.
 As we can see, users with IDs 30 and 96 visited the mall one time without making any transactions. Also, user 54 visited the mall twice and did not make any transactions.
 */
+
+CREATE DATABASE IF NOT EXISTS 1581_Customer_Who_Visited_but_Did_Not_Make_Any_Transactions;
+USE 1581_Customer_Who_Visited_but_Did_Not_Make_Any_Transactions;
  
 CREATE TABLE Visits (
     visit_id INT,
